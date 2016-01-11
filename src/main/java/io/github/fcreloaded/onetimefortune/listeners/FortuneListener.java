ce20863eeb482fc2f227da5a1c7088aeabe36459
Final Child <fcreloaded@outlook.com>
2016-01-11 18:18:33 +0900

    Version 1.1. Added permission support.

--- src/main/java/io/github/fcreloaded/onetimefortune/listeners/FortuneListener.java
+++ src/main/java/io/github/fcreloaded/onetimefortune/listeners/FortuneListener.java
@@ -28,7 +28,7 @@ import io.github.fcreloaded.onetimefortune.utils.FortuneEnchantmentUtils;
 
 /**
  * @author Final Child
- * @version 1.0
+ * @version 1.1
  * @since 1.0
  */
 public class FortuneListener implements Listener {
@@ -49,7 +49,10 @@ public class FortuneListener implements Listener {
         if (event.isCancelled()) {
             return;
         }
-        
+        if (event.getPlayer().hasPermission("onetimefortune.keep")) {
+            return;
+        }
+
         if (FortuneEnchantmentUtils.doesFortuneApply(event)) {
             FortuneRemovedEvent removedEvent = new FortuneRemovedEvent(event);
             Bukkit.getServer().getPluginManager().callEvent(removedEvent);
